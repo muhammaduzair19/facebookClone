cdf35f1e114956aa875547df83d25d4745a287b7
muhammaduzair19 <muhammaduzairilyas@gmail.com>
2023-07-06 12:23:03 +0500

    firebase integration

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-analytics.js";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBnnc0aZ1G084XQPmNBJXIdlSBt49oKLYE",
+    authDomain: "facebook-2dd3a.firebaseapp.com",
+    projectId: "facebook-2dd3a",
+    storageBucket: "facebook-2dd3a.appspot.com",
+    messagingSenderId: "1017318874658",
+    appId: "1:1017318874658:web:e19b0084c8f27a7b20b47b",
+    measurementId: "G-2H8M7STKHX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const newaccount = document.querySelector('.new-account');
 const overlay = document.querySelector('.overlay')
 const signup = document.querySelector('.signup-container')
@@ -39,9 +72,9 @@ function login() {
 
     if (!userRegistered.length) return alert("This email is not registered, please create new account")
 
-    if(userRegistered[0].password === loginpassword.value){
+    if (userRegistered[0].password === loginpassword.value) {
         localStorage.setItem('isLoggedInUser', JSON.stringify(userRegistered[0]))
-        window.location.href="./dashboard/index.html";
+        window.location.href = "./dashboard/index.html";
     }
 
 
