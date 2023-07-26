33bc62aca22a7120dfd30c294d4700612cb60417
muhammaduzair19 <muhammaduzairilyas@gmail.com>
2023-07-26 15:20:13 +0500

    firebase configure

--- app.js
+++ app.js
@@ -1,26 +1,5 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-analytics.js";
-import { getFirestore, collection, addDoc, setDoc, doc } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+import { auth, app, db, getAuth, getFirestore, createUserWithEmailAndPassword, signInWithEmailAndPassword, collection, addDoc, setDoc} from "./firebase.js";
 
-
-
-
-const firebaseConfig = {
-    apiKey: "AIzaSyBnnc0aZ1G084XQPmNBJXIdlSBt49oKLYE",
-    authDomain: "facebook-2dd3a.firebaseapp.com",
-    projectId: "facebook-2dd3a",
-    storageBucket: "facebook-2dd3a.appspot.com",
-    messagingSenderId: "1017318874658",
-    appId: "1:1017318874658:web:e19b0084c8f27a7b20b47b",
-    measurementId: "G-2H8M7STKHX"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth(app);
-const db = getFirestore(app);
 
 
 
---
+++ firebase.js
@@ -0,0 +1,36 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-analytics.js";
+import { getFirestore, collection, addDoc, setDoc, doc } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBnnc0aZ1G084XQPmNBJXIdlSBt49oKLYE",
+    authDomain: "facebook-2dd3a.firebaseapp.com",
+    projectId: "facebook-2dd3a",
+    storageBucket: "facebook-2dd3a.appspot.com",
+    messagingSenderId: "1017318874658",
+    appId: "1:1017318874658:web:e19b0084c8f27a7b20b47b",
+    measurementId: "G-2H8M7STKHX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export {
+    app,
+    auth,
+    db,
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    getFirestore,
+    collection,
+    addDoc,
+    setDoc,
+    doc
+}
